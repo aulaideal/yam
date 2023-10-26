dacd4a3670d22276e4cd8a599f12a8f037807006
Erick Omar Mines <aulaideal@gmail.com>
2023-10-26 11:45:14 -0500

    v1.9

--- yam.php
+++ yam.php
@@ -2,18 +2,15 @@
 
 /**
  * Plugin Name: Yam
- * Plugin URI: http://thimpress.com/learnpress
+ * Plugin URI: http://aulaideal.com
  * Description: LearnPress is a WordPress complete solution for creating a Learning Management System (LMS). It can help you to create courses, lessons and quizzes.
  * Author: ThimPress2
- * Version: 1.8
-
+ * Version: 1.9
  * Requires at least: 5.8
  * Tested up to: 6.1.1
  * Requires PHP: 7.0
- * Text Domain:Yam
-
+ * Text Domain:yam
  */
-
 
 require 'plugin-update-checker/plugin-update-checker.php';
 
@@ -27,25 +24,3 @@ $myUpdateChecker = PucFactory::buildUpdateChecker(
 
 //Set the branch that contains the stable release.
 $myUpdateChecker->setBranch('main');
-
-
-
-/* public function plugin_setup()
-{
-add_filter( 'upgrader_source_selection', array( $this, 'rename_github_zip' ), 1, 3);
-}
-
-
-public function rename_github_zip( $source, $remote_source, $thiz )
-{
-if( strpos( $source, 'github-plugin-for-wordpress') === false )
-return $source;
-
-$path_parts = pathinfo( $source );
-$newsource = trailingslashit( $path_parts['dirname'] ) . trailingslashit( 'github-plugin-for-wordpress' );
-rename( $source, $newsource );
-return $newsource;
-} */
-?>
-
-<h1>HOL</h1>
