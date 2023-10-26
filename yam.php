9b733487b51069e0a7846540ec50dd8444c34549
Erick Omar Mines <aulaideal@gmail.com>
2023-10-26 11:38:37 -0500

    v1.8

--- yam.php
+++ yam.php
@@ -5,7 +5,7 @@
  * Plugin URI: http://thimpress.com/learnpress
  * Description: LearnPress is a WordPress complete solution for creating a Learning Management System (LMS). It can help you to create courses, lessons and quizzes.
  * Author: ThimPress2
- * Version: 1.7
+ * Version: 1.8
 
  * Requires at least: 5.8
  * Tested up to: 6.1.1
